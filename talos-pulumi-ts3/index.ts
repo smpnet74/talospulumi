2ef7b5a940dc9e9e4a1fc8be2e8910c7cc9c4653
Scott Peterson <sm_peterson@yahoo.com>
2022-05-13 20:06:08 +0000

    add ebs volumes for rook and ceph

--- talos-pulumi-ts3/index.ts
+++ talos-pulumi-ts3/index.ts
@@ -2,6 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import { local } from "@pulumi/command";
 import { interpolate } from "@pulumi/pulumi";
+import { Volume } from "@pulumi/aws/ebs";
 
 /** This stack is responsible for building the EC2 from the talosconfig in stack talos-pulumi-ts2 */
 const config = new pulumi.Config();
@@ -15,8 +16,12 @@ const VPCid = stackRef.getOutput("VPCid");
 
 const controlUD = new local.Command("controlUD", {create: `cat ../talos-pulumi-ts2/talosconfig/controlplane.yaml`})
 const workerUD = new local.Command("workerUD", {create: `cat ../talos-pulumi-ts2/talosconfig/worker.yaml`})
+const serverips: any[] = []
+const workerips: any[] = []
+const ebsids: any[] = []
 const serverids: any[] = []
 const workerids: any[] = []
+const count = 0
 
 for (let i = 0; i < 3; i++) {
     const server = new aws.ec2.Instance(`talos-control-${i}`, {
@@ -30,7 +35,8 @@ for (let i = 0; i < 3; i++) {
     },
     {ignoreChanges: ["securityGroups"]}
     );
-    serverids.push(server.privateIp)
+    serverips.push(server.privateIp)
+    serverids.push(server.id)
 };
 
 for (let i = 0; i < 3; i++) {
@@ -44,7 +50,21 @@ for (let i = 0; i < 3; i++) {
         tags: { "Name": `talos-worker-${i}` },
     },
     {ignoreChanges: ["securityGroups"]});
-    workerids.push(server.privateIp)
+    workerips.push(server.privateIp)
+    workerids.push(server.id)
+};
+
+for (let i = 0; i < 3; i++) {
+    const example = new aws.ebs.Volume(`example-${i}`, {
+        availabilityZone: "us-east-1a",
+        size: 100,
+    });
+    const ebsattach = new aws.ec2.VolumeAttachment(`ebsatt-${i}`, {
+        deviceName: "/dev/sdh",
+        volumeId: example.id,
+        instanceId: workerids.pop()
+    })
+    ebsids.push(example.id)
 };
 
 const lbtarget = new aws.alb.TargetGroup("lbtarget", {
@@ -56,6 +76,6 @@ const lbtarget = new aws.alb.TargetGroup("lbtarget", {
 })
 
 export const targetgrouparn = lbtarget.arn
-export const cp1 = serverids[0]
-export const cp2 = serverids[1]
-export const cp3 = serverids[2]
+export const cp1 = serverips[0]
+export const cp2 = serverips[1]
+export const cp3 = serverips[2]
